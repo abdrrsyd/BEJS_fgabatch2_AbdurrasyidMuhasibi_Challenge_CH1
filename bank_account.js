6858017777dc9056d6bd1746a975adc21518d548
Abdurrasyid Muhasibi <com.syid@gmail.com>
2024-08-05 12:35:00 +0700

    add function reduceBalance()

--- bank_account.js
+++ bank_account.js
@@ -12,3 +12,18 @@ function addBalance() {
     window.alert("The amount entered is invalid. Please try again.");
   }
 }
+
+function reduceBalance() {
+  let amount = parseFloat(
+    window.prompt("Enter the amount of balance you want to reduce:")
+  );
+
+  if (!isNaN(amount) && amount > 0 && amount <= balance) {
+    balance -= amount;
+    window.alert(`Your new balance is: ${balance}`);
+  } else if (amount > balance) {
+    window.alert("Balance is not enough to reduce the amount.");
+  } else {
+    window.alert("The amount entered is invalid. Please try again.");
+  }
+}
