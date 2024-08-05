04cc78e3af0cc936602b6239230ccca18c2b2635
Abdurrasyid Muhasibi <com.syid@gmail.com>
2024-08-05 12:32:30 +0700

    add function addBalance()

--- bank_account.js
+++ bank_account.js
@@ -1,1 +1,14 @@
 let balance = 0;
+
+function addBalance() {
+  let amount = parseFloat(
+    window.prompt("Enter the amount of balance you want to add:")
+  );
+
+  if (!isNaN(amount) && amount > 0) {
+    balance += amount;
+    window.alert(`Your new balance is: ${balance}`);
+  } else {
+    window.alert("The amount entered is invalid. Please try again.");
+  }
+}
